1b6450dc3539fda448a00c9869fa123cbc2a6151
DanBaDo <byte4life@pm.me>
2022-05-06 10:57:19 +0200

    Tests work in progress...

--- test/index.mjs
+++ test/index.mjs
@@ -4,11 +4,12 @@ import request from "supertest";
 
 import chai, { expect } from "chai";
 import chaiThings from "chai-things";
-import chaiJsonSchema from "chai-json-schema";
+import chaiJsonSchemaAjv from "chai-json-schema-ajv"
 chai.use(chaiThings);
-chai.use(chaiJsonSchema);
+chai.use(chaiJsonSchemaAjv);
 
 import { getAllTasksController } from '../src/controllers/tasksControllers.mjs';
+import { taskSchema, newTaskSchema } from "../src/schemas/tasksSchemas.mjs";
 
 describe('tasksControllers testing', function() {
 
@@ -23,16 +24,31 @@ describe('tasksControllers testing', function() {
     });
   });
 
-  it('', function() {
+  it('has Content-Type application/json', function() {
     request(app)
       .get('/')
-      .set('Content-Type', 'application/json')
       .expect('Content-Type', /json/)
-      .expect(200, function(err, res) {
-        if (err) { return done(err); }
-        expect(res.body)
-          .to.be.an("array")
-          .that.contains.something.that.to.be.jsonSchema(fruitSchema)
+  });
+
+  it('has response status code 200', function() {
+    request(app)
+      .get('/')
+      .expect(200)
+  });
+
+  it('body is a JSON array', function() {
+    request(app)
+      .get('/')
+      .end(function(err, res) {
+        expect(res.body).to.be.an("array")
+      });
+  });
+
+  it('body array elements fits task JSON schema', function() {
+    request(app)
+      .get('/')
+      .end(function(err, res) {
+        expect(res.body).contains.something.that.to.be.jsonSchema(taskSchema)
       });
   });
 
